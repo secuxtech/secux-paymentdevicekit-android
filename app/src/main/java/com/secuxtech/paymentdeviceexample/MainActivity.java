b1a70942155c9ebd0a1bb2b2557b21553c6a17c4
maochunsun <maochuns.sun@gmail.com>
2020-03-04 16:32:43 +0800

    Update MainActivity.java

--- app/src/main/java/com/secuxtech/paymentdeviceexample/MainActivity.java
+++ app/src/main/java/com/secuxtech/paymentdeviceexample/MainActivity.java
@@ -29,8 +29,23 @@ import com.secuxtech.paymentdevicekit.SecuXBLEManager;
 
 import org.json.JSONObject;
 
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.AlgorithmParameterSpec;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import static com.secuxtech.paymentdevicekit.PaymentPeripheralManager.SecuX_Peripheral_Operation_OK;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -82,52 +97,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                PaymentPeripheralManager peripheralManager = new PaymentPeripheralManager();
-                peripheralManager.doGetIVKey(mContext, 10000, "4ab10000726b", -80, 10000);
-                //peripheralManager.doGetIVKey(mContext, 10000, "001a00000048", -80, 10000);
-
-                try {
-                    JSONObject ioCtrlParamJson = new JSONObject("{\"uart\":\"0\",\"gpio1\":\"0\",\"gpio2\":\"0\",\"gpio31\":\"0\",\"gpio32\":\"0\",\"gpio4\":\"0\",\"gpio4c\":\"0\",\"gpio4cInterval\":\"0\",\"gpio4cCount\":\"0\",\"gpio4dOn\":\"0\",\"gpio4dOff\":\"0\",\"gpio4dInterval\":\"0\",\"runStatus\":\"0\",\"lockStatus\":\"0\"}");
-
-                    final MachineIoControlParam machineIoControlParam = new MachineIoControlParam();
-                    machineIoControlParam.setGpio1(ioCtrlParamJson.getString("gpio1"));
-                    machineIoControlParam.setGpio2(ioCtrlParamJson.getString("gpio2"));
-                    machineIoControlParam.setGpio31(ioCtrlParamJson.getString("gpio31"));
-                    machineIoControlParam.setGpio32(ioCtrlParamJson.getString("gpio32"));
-                    machineIoControlParam.setGpio4(ioCtrlParamJson.getString("gpio4"));
-                    machineIoControlParam.setGpio4c(ioCtrlParamJson.getString("gpio4c"));
-                    machineIoControlParam.setGpio4cCount(ioCtrlParamJson.getString("gpio4cCount"));
-                    machineIoControlParam.setGpio4cInterval(ioCtrlParamJson.getString("gpio4cInterval"));
-                    machineIoControlParam.setGpio4dOn(ioCtrlParamJson.getString("gpio4dOn"));
-                    machineIoControlParam.setGpio4dOff(ioCtrlParamJson.getString("gpio4dOff"));
-                    machineIoControlParam.setGpio4dInterval(ioCtrlParamJson.getString("gpio4dInterval"));
-                    machineIoControlParam.setUart(ioCtrlParamJson.getString("uart"));
-                    machineIoControlParam.setRunStatus(ioCtrlParamJson.getString("runStatus"));
-                    machineIoControlParam.setLockStatus(ioCtrlParamJson.getString("lockStatus"));
-
-                    String encryptedStr = "91sGnngVALh6Ep3RsEJKhGQEQM2ntJiZxR0cwiQNLT\\/SbZcCVux1WHabIrzqkICsPz3PudpRHnEFwcbiMO7qhA==";
-                    final byte[] encryptedData = Base64.decode(encryptedStr, Base64.DEFAULT);
-                    Pair<Integer, String> ret = peripheralManager.doPaymentVerification(encryptedData, machineIoControlParam);
-                    if (ret.first != 0) {
-                        Log.i(TAG, ret.second);
-                    }else{
-                        Log.i(TAG, "Payment done");
-                    }
-                }catch (Exception e){
-
-                }
-            }
-        }).start();
-
-
-
-
-
-
-/*
 
         SecuXBLEManager.getInstance().startScan();
 
@@ -135,10 +104,84 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL));
 
 
- */
 
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+    public byte[] getEncryptMobilePaymentCommand(String terminalId, String amount, String ivKey, String cryptKey){
+        Log.d(TAG,"getEncryptMobilePaymentCommand()");
+        String plainTransaction = getMobilePaymentCommand(terminalId, amount);
+
+        // AES 256 crypt
+        byte[] encrytedTransactionData = null;
+        try {
+            byte[] ivKeyData = ivKey.getBytes();
+            encrytedTransactionData = encrypt(ivKeyData, cryptKey.getBytes(), plainTransaction.getBytes());
+        }catch (Exception ex) {
+            encrytedTransactionData = null;
+        }
+
+        return encrytedTransactionData;
+    }
+
+    public byte[] encrypt(byte[] ivBytes, byte[] keyBytes, byte[] textBytes)
+            throws java.io.UnsupportedEncodingException,
+            NoSuchAlgorithmException,
+            NoSuchPaddingException,
+            InvalidKeyException,
+            InvalidAlgorithmParameterException,
+            IllegalBlockSizeException,
+            BadPaddingException {
+
+        AlgorithmParameterSpec ivSpec = new IvParameterSpec(ivBytes);
+        SecretKeySpec newKey = new SecretKeySpec(keyBytes, "AES");
+        Cipher cipher = null;
+        cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, newKey, ivSpec);
+        return cipher.doFinal(textBytes);
+    }
+
+    public String getMobilePaymentCommand(String terminalId, String amount) {
+        Log.d(TAG,"======= getMobilePaymentCommand() =======");
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat format1 = new SimpleDateFormat("yyyyMMddHHmmss");
+        String transactionTime = format1.format(c.getTime());
+        String paymentID = "P12345678912" + transactionTime.substring(transactionTime.length() - 4);
+        String amountString = convertAmountToFourDigits(amount);
+        String amountCurrency = "DCT";
+        if(amountCurrency.length() == 0) amountCurrency = "TWD";
+        String plainTransaction = transactionTime + "," + paymentID + "," + terminalId + "," + amountString+","+amountCurrency;
+
+        Log.d(TAG,"plainTransaction:"+plainTransaction);
+        return plainTransaction;
+    }
+
+    private String convertAmountToFourDigits(String amount) {
+        int length = String.valueOf(amount).length();
+
+        String amountStr = amount+"";
+        int Remaining = 8-length;
+        for(int i=0;i<Remaining;i++){
+            amountStr = " "+amountStr;
+        }
+
+//        if (length == 1) {
+//            return "   " + amount;
+//        }
+//
+//        if (length == 2) {
+//            return "  " + amount;
+//        }
+//
+//        if (length == 3) {
+//            return " " + amount;
+//        }
+
+        // length is 8
+        Log.d(TAG,"convertAmountToFourDigits: "+amountStr);
+        Log.d(TAG,"convertAmountToFourDigits: "+amountStr.length());
+        return amountStr;
     }
 
     public void onRescanButtonClick(View v){
@@ -148,6 +191,78 @@ public class MainActivity extends AppCompatActivity {
         mAdapter.notifyDataSetChanged();
 
         SecuXBLEManager.getInstance().startScan();
+    }
+
+    public boolean payToDevice(List<PaymentDevice> devList, String amount){
+        boolean payToDevRet = false;
+        for (PaymentDevice dev : devList) {
+            if (!dev.deviceSelected) {
+                //Log.i(TAG, "Test " + dev.paymentDev.deviceID);
+                continue;
+            }
+
+            final String devID = dev.paymentDev.deviceID;
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    CommonProgressDialog.setProgressSubTip(devID);
+                }
+            });
+
+            PaymentPeripheralManager peripheralManager = new PaymentPeripheralManager();
+            //peripheralManager.doGetIVKey(mContext, 10000, "4ab10000726b", -80, 10000);
+            Pair<Integer, String> getIVKeyret = peripheralManager.doGetIVKeyWithoutStartScan(mContext, 10, devID, -80, 10);
+            String ivKey = "";
+            if (getIVKeyret.first == SecuX_Peripheral_Operation_OK) {
+                ivKey = getIVKeyret.second;
+                Log.i(TAG, "ivKey=" + ivKey);
+            } else {
+                Log.i(TAG, "Get ivKey failed");
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        CommonProgressDialog.dismiss();
+                    }
+                });
+                return false;
+            }
+
+            try {
+                JSONObject ioCtrlParamJson = new JSONObject("{\"uart\":\"0\",\"gpio1\":\"0\",\"gpio2\":\"0\",\"gpio31\":\"0\",\"gpio32\":\"0\",\"gpio4\":\"0\",\"gpio4c\":\"0\",\"gpio4cInterval\":\"0\",\"gpio4cCount\":\"0\",\"gpio4dOn\":\"0\",\"gpio4dOff\":\"0\",\"gpio4dInterval\":\"0\",\"runStatus\":\"0\",\"lockStatus\":\"0\"}");
+
+                final MachineIoControlParam machineIoControlParam = new MachineIoControlParam();
+                machineIoControlParam.setGpio1(ioCtrlParamJson.getString("gpio1"));
+                machineIoControlParam.setGpio2(ioCtrlParamJson.getString("gpio2"));
+                machineIoControlParam.setGpio31(ioCtrlParamJson.getString("gpio31"));
+                machineIoControlParam.setGpio32(ioCtrlParamJson.getString("gpio32"));
+                machineIoControlParam.setGpio4(ioCtrlParamJson.getString("gpio4"));
+                machineIoControlParam.setGpio4c(ioCtrlParamJson.getString("gpio4c"));
+                machineIoControlParam.setGpio4cCount(ioCtrlParamJson.getString("gpio4cCount"));
+                machineIoControlParam.setGpio4cInterval(ioCtrlParamJson.getString("gpio4cInterval"));
+                machineIoControlParam.setGpio4dOn(ioCtrlParamJson.getString("gpio4dOn"));
+                machineIoControlParam.setGpio4dOff(ioCtrlParamJson.getString("gpio4dOff"));
+                machineIoControlParam.setGpio4dInterval(ioCtrlParamJson.getString("gpio4dInterval"));
+                machineIoControlParam.setUart(ioCtrlParamJson.getString("uart"));
+                machineIoControlParam.setRunStatus(ioCtrlParamJson.getString("runStatus"));
+                machineIoControlParam.setLockStatus(ioCtrlParamJson.getString("lockStatus"));
+
+                //String encryptedStr = "91sGnngVALh6Ep3RsEJKhGQEQM2ntJiZxR0cwiQNLT\\/SbZcCVux1WHabIrzqkICsPz3PudpRHnEFwcbiMO7qhA==";
+                //final byte[] encryptedData = Base64.decode(encryptedStr, Base64.DEFAULT);
+                final byte[] encryptedData = getEncryptMobilePaymentCommand(devID.substring(devID.length() - 8, devID.length()), amount, ivKey, "PA123456789012345678901234567890");
+
+                Pair<Integer, String> ret = peripheralManager.doPaymentVerification(encryptedData, machineIoControlParam);
+                if (ret.first != 0) {
+                    Log.i(TAG, "Payment failed! " + ret.second);
+                } else {
+                    Log.i(TAG, "Payment done");
+                    payToDevRet = true;
+                }
+            } catch (Exception e) {
+                Log.i(TAG, "Generate io configuration failed!");
+            }
+        }
+        return payToDevRet;
     }
 
 
@@ -183,6 +298,7 @@ public class MainActivity extends AppCompatActivity {
                 while(true){
                     //Log.i(TAG, "Round " + i);
 
+                    /*
                     for (PaymentDevice dev : devList) {
                         if (dev.deviceSelected) {
                             Log.i(TAG, "Test " + dev.paymentDev.deviceID);
@@ -195,7 +311,7 @@ public class MainActivity extends AppCompatActivity {
                             });
 
                             Pair<Integer, String> ret = mPeripheralManager.doGetIVKeyWithoutStartScan(mContext, 10000, dev.paymentDev.deviceID, -80, 20000);
-                            if (ret.first != PaymentPeripheralManager.SecuX_Peripheral_Operation_OK) {
+                            if (ret.first != SecuX_Peripheral_Operation_OK) {
                                 final PaymentDevice pdev = dev;
                                 final String errorMsg = ret.second;
                                 runOnUiThread(new Runnable() {
@@ -215,6 +331,13 @@ public class MainActivity extends AppCompatActivity {
 
                         if (!mTestRun)
                             break;
+                    }
+
+
+                     */
+
+                    if (!payToDevice(devList, String.valueOf(idx))){
+                        break;
                     }
 
 
@@ -246,6 +369,8 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         }).start();
+
+
     }
 
     public void onStopTestButtonClick(View v) {
